72c24bde0331b3e396a074afe1313ec7d925f2b9
Dmitry Panov <139315134+weistum@users.noreply.github.com>
2023-07-12 21:07:47 +0300

    Лаборатораная работа 1

---
+++ Лабораторная работа 1/Task_3.py
@@ -0,0 +1,7 @@
+list_players = ["Маша", "Петя", "Саша", "Оля", "Кирилл", "Коля"]
+
+# TODO Разделите участников на две команды
+player_in_team = round(len(list_players)/2)  # Определение количества игроков в команде
+
+print(list_players[:player_in_team])
+print(list_players[-player_in_team:])
